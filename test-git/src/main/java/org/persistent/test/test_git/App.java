ee5de038d65be2225d07fb91433fcf306bbf746a
vandana_zanzad <vandana_zanzad@HJD23909.persistent.co.in>
2019-05-08 11:29:10 +0530

    First commit

---
+++ test-git/src/main/java/org/persistent/test/test_git/App.java
@@ -0,0 +1,13 @@
+package org.persistent.test.test_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
